4551b7c70b91125bcd998e59c1999139b283d9f3
Luis Cisneros <tiko.cisneros@gmail.com>
2023-06-22 23:57:23 -0500

    feat: add notes reducer cases

--- apps/notes-client/src/store/middleware/api.ts
+++ apps/notes-client/src/store/middleware/api.ts
@@ -1,8 +1,17 @@
+/**
+ * https://patrickdesjardins.com/blog/how-to-create-a-typed-redux-middleware-in-typescript
+ * https://www.programcreek.com/typescript/?api=redux.Middleware
+ * https://www.smashingmagazine.com/2023/05/guide-redux-toolkit-typescript/
+ * https://blog.logrocket.com/using-typescript-redux-toolkit/
+ */
+
 import { AnyAction, Dispatch, Middleware } from '@reduxjs/toolkit';
 import { fetchHttp } from '../actions/api';
 
 const apiMiddleware: Middleware =
-  (_store) => (next: Dispatch<AnyAction>) => async (action: AnyAction) => {
+  ({ dispatch }) =>
+  (next: Dispatch<AnyAction>) =>
+  async (action: AnyAction) => {
     const { type } = action;
     if (type !== fetchHttp.type) {
       next(action);
@@ -10,6 +19,7 @@ const apiMiddleware: Middleware =
     }
 
     // TODO: Add api behavior
+    dispatch(action);
   };
 
 export default apiMiddleware;
--- apps/notes-client/src/store/notes.ts
+++ apps/notes-client/src/store/notes.ts
@@ -1,12 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { CaseReducer, PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { INote } from '@simple-notes/models';
 
-const INITIAL_STATE: INote[] = [];
+export type TNotesState = INote[];
+
+const INITIAL_STATE: TNotesState = [];
+
+const populateNotes: CaseReducer<TNotesState, PayloadAction<TNotesState>> = (
+  state,
+  action
+) => {
+  state = action.payload;
+};
+
+const addNote: CaseReducer<TNotesState, PayloadAction<INote>> = (
+  state,
+  action
+) => {
+  state = [...state, action.payload];
+};
+
+const updateNote: CaseReducer<TNotesState, PayloadAction<INote>> = (
+  state,
+  action
+) => {
+  const index = state.findIndex(({ id }) => id === action.payload.id);
+  state[index] = action.payload;
+};
+
+const removeNote: CaseReducer<TNotesState, PayloadAction<INote>> = (
+  state,
+  action
+) => {
+  const index = state.findIndex(({ id }) => id === action.payload.id);
+  state.splice(index, 1);
+};
 
 const notesSlide = createSlice({
   name: 'notes',
   initialState: INITIAL_STATE,
-  reducers: {},
+  reducers: {
+    populateNotes,
+    addNote,
+    updateNote,
+    removeNote,
+  },
 });
 
 export default notesSlide.reducer;
